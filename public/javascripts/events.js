4cbb3c3454924fa46bb043f59e58fa1394a8f885
aschuler <adrien.schuler@gmail.com>
2012-04-11 15:28:37 +0200

    updated events

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -1,3 +1,10 @@
-$(window).bind('orientationchange', function(e){
-	console.log( 'orientation: ' + e.orientation );
+$(function(){
+
+	console.log('triggering application events');
+
+	$(window).bind('orientationchange', function(e){
+		console.log( 'orientation: ' + e.orientation );
+	});
+
 });
+
