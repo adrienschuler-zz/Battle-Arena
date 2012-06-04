b9770ffbafd0c6fc1d9d103ff3f8d841fe2caa3c
aschuler <adrien.schuler@gmail.com>
2012-06-04 23:49:33 +0200

    removed session_controller

--- config/routes.js
+++ config/routes.js
@@ -5,8 +5,7 @@
 module.exports = function(app, io, sessionStore) {
 
 	// Controllers
-	var session = require('../app/controllers/session_controller')(app, io)
-		, user 		= require('../app/controllers/user_controller')(app)
+	var user 		= require('../app/controllers/user_controller')(app)
 		, game 		= require('../app/controllers/game_controller')(app, io, sessionStore);
 
 
